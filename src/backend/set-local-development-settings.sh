8220fb21a82781c9845fa0a7e1ed06d41aad51a9
James Eby <temporafugiunt@gmail.com>
2019-06-27 17:33:06 -0500

    Updated more files to App2 vs. App1.

--- src/backend/set-local-development-settings.sh
+++ src/backend/set-local-development-settings.sh
@@ -4,7 +4,7 @@ cp -r ../build/* ./client
 
 export BUILD_VERSION=0.1.1.1
 export ENVIRONMENT_NAME=localdev
-export APP_NAME=App1
+export APP_NAME=App2
 export PORT=3000
 
 # Build the client code and place it in /client/api1 in this directory.
--- src/set-local-development-settings.sh
+++ src/set-local-development-settings.sh
@@ -1,6 +1,4 @@
 export BUILD_VERSION=0.1.1.1
-export ENVIRONMENT_NAME=localdev
-export PUBLIC_URL=http://localhost:3000/api2
 export APP_NAME=App2
 
 npm run start
